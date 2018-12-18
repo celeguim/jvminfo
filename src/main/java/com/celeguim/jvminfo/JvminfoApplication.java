fa4850ab231ac898eea2c5ed0c210d1592939ccb
celeguim <celeguim@localhost.localdomain>
2018-12-18 21:25:39 +0100

    add docker

--- src/main/java/com/celeguim/jvminfo/JvminfoApplication.java
+++ src/main/java/com/celeguim/jvminfo/JvminfoApplication.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class JvminfoApplication {
 
     @RequestMapping("/")
-    String root() {
+    String root(HttpSession session) {
+        String sessionId = session.getId();
+
         String hostname = null;
         String hostaddress = null;
         try {
             hostname = InetAddress.getLocalHost().getHostName();
             hostaddress = InetAddress.getLocalHost().getHostAddress();
+            
         } catch (UnknownHostException ex) {
             Logger.getLogger(JvminfoApplication.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return "Hostname: " + hostname + "</br>Hostaddress: " + hostaddress;
+        
+        session.invalidate();
+        String server=session.getServletContext().getServerInfo();
+        //System.out.println(session.getServletContext().getServerInfo());
+        
+        
+        return "Session Id: " + sessionId + "</br>" + 
+                "Server: " + server + "</br>" + 
+                "Hostname: " + hostname + "</br>" + 
+                "Hostaddress: " + hostaddress;
     }
 
     public static void main(String[] args) {
--- src/test/java/com/celeguim/jvminfo/JvminfoApplicationTests.java
+++ src/test/java/com/celeguim/jvminfo/JvminfoApplicationTests.java
@@ -2,11 +2,13 @@ package com.celeguim.jvminfo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootConfiguration
 public class JvminfoApplicationTests {
 
 	@Test
