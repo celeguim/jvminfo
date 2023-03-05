0af8d75894ca692918f65a90ea1b51244b5886a8
Luiz H. Celeguim <luiz.celeguim@gmail.com>
2023-03-06 00:15:50 +0100

    added servlet jvminfo / cookies

---
+++ src/main/java/com/celeghin/jvminfo/JvmInfo.java
@@ -0,0 +1,55 @@
+package com.celeghin.jvminfo;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class JvmInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public JvmInfo() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession(true);
+		Cookie myCookie = new Cookie("MY_COOKIE", session.getId());
+		response.addCookie(myCookie);
+
+		InetAddress ip = null;
+		String hostname;
+		try {
+			ip = InetAddress.getLocalHost();
+			hostname = ip.getHostName();
+			System.out.println("Your current IP address : " + ip);
+			System.out.println("Your current Hostname : " + hostname);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(String.format("<table border='1'>"));
+		builder.append(String.format("<tr><td width='150'> Hostname/IP </td>"));
+		builder.append(String.format("<td width='150'> %s </td></tr>", ip));
+		builder.append(String.format("<tr><td width='150'> %s </td>", myCookie.getName()));
+		builder.append(String.format("<td width='150'> %s </td></tr>", myCookie.getValue()));
+		builder.append("</table>");
+		response.getWriter().append(builder.toString());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
