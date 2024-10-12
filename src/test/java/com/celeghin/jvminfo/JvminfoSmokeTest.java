2479e6bd7e7648483afe7fab302f23d21c6ae237
Luiz Celeghin <luiz.celeguim@gmail.com>
2024-10-12 19:21:14 +0200

    smoketest

--- src/test/java/com/celeghin/jvminfo/JvminfoSmokeTest.java
+++ src/test/java/com/celeghin/jvminfo/JvminfoSmokeTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-// import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,18 +17,18 @@ class JvminfoSmokeTest {
 	@Autowired
 	private JvmInfoController jvmInfoController;
 
-	// @Autowired
-	// private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() throws Exception {
 		assertNotNull(jvmInfoController);
 	}
 
-	// @Test
-	// public void testStatusResponse() throws Exception {
-	// 	mockMvc.perform(MockMvcRequestBuilders.get("/"))
-	// 			.andExpect(MockMvcResultMatchers.status().isOk());
-	// }
+	@Test
+	public void testStatusResponse() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 
 }
