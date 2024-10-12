fa6aa0949804e4bd1d6178e5c75a965154457f63
Luiz Celeghin <luiz.celeguim@gmail.com>
2024-10-12 18:53:37 +0200

    update smoketest

--- src/test/java/com/celeghin/jvminfo/JvminfoSmokeTest.java
+++ src/test/java/com/celeghin/jvminfo/JvminfoSmokeTest.java
@@ -1,14 +1,17 @@
 package com.celeghin.jvminfo;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JvminfoSmokeTest {
 
 	@Autowired
