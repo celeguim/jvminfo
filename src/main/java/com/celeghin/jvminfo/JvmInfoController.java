60c8993e9673ebef81218fbf3a75299bda88ad49
Luiz Celeghin <luiz.celeguim@gmail.com>
2025-11-17 23:34:35 -0300

    updates.

--- src/main/java/com/celeghin/jvminfo/JvmInfoController.java
+++ src/main/java/com/celeghin/jvminfo/JvmInfoController.java
@@ -3,15 +3,13 @@ package com.celeghin.jvminfo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.boot.ApplicationArguments;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.Disposable;
-import reactor.core.publisher.Mono;
 
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
@@ -25,23 +23,33 @@ import java.util.List;
 public class JvmInfoController {
 
     private final ApplicationArguments applicationArguments;
+    private final JvmInfoHealthDepStatus dependencies;
 
     public JvmInfoController(ApplicationArguments applicationArguments, JvmInfoHealthDepStatus dependencies) {
         this.applicationArguments = applicationArguments;
         this.dependencies = dependencies;
     }
 
-    private final JvmInfoHealthDepStatus dependencies;
-
-    @GetMapping(value = "/")
+    @GetMapping("/")
     String getHome(ModelMap model) {
+        boolean dbUp = dependencies.isDbUp();
+        boolean rabbitUp = dependencies.isRabbitUp();
+        model.addAttribute("jvmInfoObj", getModel(dbUp, rabbitUp));
+        return "index";
+    }
 
-        boolean dbUp = true;
-        boolean rabbitUp = false;
+    @PostMapping("/")
+    String refreshHome(ModelMap model,
+                       @RequestParam(name = "dbState", required = false) boolean dbState,
+                       @RequestParam(name = "rabbitState", required = false) boolean rabbitState
+                       ) {
+        dependencies.setDbUp(dbState);
+        dependencies.setRabbitUp(rabbitState);
+        model.addAttribute("jvmInfoObj", getModel(dbState, rabbitState));
+        return "index";
+    }
 
-        dependencies.setDbUp(dbUp);
-        dependencies.setRabbitUp(rabbitUp);
-
+    JvmInfoModel getModel(boolean dbUp, boolean rabbitUp) {
         long mb = 1024 * 1024;
         Runtime runtime = Runtime.getRuntime();
         long usedMem = ((runtime.totalMemory() - runtime.freeMemory()) / mb);
@@ -79,9 +87,6 @@ public class JvmInfoController {
         HttpServletRequest request = attr.getRequest();
 
         JvmInfoModel jvmInfoObj = new JvmInfoModel();
-
-        jvmInfoObj.dbState = dbUp;
-        jvmInfoObj.rabbitState = rabbitUp;
 
         jvmInfoObj.country = String.format("%s / %s",
                 request.getLocale().getCountry(), request.getLocale().getDisplayCountry());
@@ -122,10 +127,11 @@ public class JvmInfoController {
         jvmInfoObj.timezone = calendar.getTimeZone().getDisplayName();
         jvmInfoObj.appArgs = appArgs.toString();
 
-        model.addAttribute("jvmInfoObj", jvmInfoObj);
+        jvmInfoObj.dbState = dbUp;
+        jvmInfoObj.rabbitState = rabbitUp;
+
         session.invalidate();
 
-        return "index";
+        return jvmInfoObj;
     }
-
 }
